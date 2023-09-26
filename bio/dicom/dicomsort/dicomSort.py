ec09270a3c55c65d39e3b29ae882eb0d2b9916b4
Jermiah <jermiahjoseph@gmail.com>
2023-09-26 17:12:00 +0000

    first commit

---
+++ bio/dicom/dicomsort/dicomSort.py
@@ -0,0 +1,17 @@
+# FILEPATH: wrapper.py
+
+# Import the necessary modules
+from snakemake.shell import shell
+
+# Define the input, output, and parameters
+input_files = snakemake.input.input_files
+output_dir = snakemake.output.output_dir
+params = snakemake.params
+
+# Define the command to be executed
+command = "dicomsort {input_files} {output_dir} {params}"
+
+# Execute the command
+shell(command)
+
+
